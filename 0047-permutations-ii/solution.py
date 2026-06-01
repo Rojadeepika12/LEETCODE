61e0b5e286c72a647dfcef9074516964a70531f6
Rojadeepika12 <23h41a4559@bvcits.edu.in>
2026-06-01 05:48:50 +0000

    Sync LeetCode submission Runtime - 9 ms (25.55%), Memory - 19.5 MB (87.40%)

---
+++ 0047-permutations-ii/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        from itertools import permutations
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        perm=permutations(nums)
+        return list(set(perm))
+        
